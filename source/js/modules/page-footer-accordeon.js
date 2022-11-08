ad8516941145e42d7c95181e15d3d777588c233f
hatter-j <hatterjulia@mail.ru>
2022-11-09 01:33:59 +0300

    Главный экран, преимущества

--- source/js/modules/page-footer-accordeon.js
+++ source/js/modules/page-footer-accordeon.js
@@ -1,36 +1,20 @@
 const siteSections = document.querySelector('.site-sections');
 const siteSectionsToggle = siteSections.querySelector('.site-sections__toggle');
 const contacts = document.querySelector('.contacts');
-const contactsToggle = siteSections.querySelector('.contacts__toggle');
+const contactsToggle = contacts.querySelector('.contacts__toggle');
 
 const toggleSiteSections = () => {
-  siteSectionsToggle.addEventListener('click', () => {
-    siteSections.setAttribute('data-site-sections', 'is-opened');
-  });
-  /*
   const value = siteSections.getAttribute('data-site-sections');
 
   siteSectionsToggle.addEventListener('click', () => {
     if (value === 'is-closed') {
+      siteSections.removeAttribute('is-closed');
       siteSections.setAttribute('data-site-sections', 'is-opened');
-    }
-    if (value === 'is-opened') {
+    } else {
+      siteSections.removeAttribute('is-opened');
       siteSections.setAttribute('data-site-sections', 'is-closed');
     }
-  });*/
-/*
-  if (value === 'is-closed') {
-    siteSectionsToggle.addEventListener('click', () => {
-      siteSections.removeAttribute('data-site-sections', 'is-closed');
-      siteSections.setAttribute('data-site-sections', 'is-opened');
-    });
-  }
-  if (value === 'is-opened') {
-    siteSectionsToggle.addEventListener('click', () => {
-      siteSections.removeAttribute('data-site-sections', 'is-opened');
-      siteSections.setAttribute('data-site-sections', 'is-closed');
-    });
-  }*/
+  });
 };
 
 const toggleContacts = () => {
